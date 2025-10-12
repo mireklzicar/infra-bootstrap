fdcb6d5837e962b76bf4e453f452a08fdfb200d6
Miroslav Lžičař <miroslav.lzicar@themama.ai>
2025-10-13 00:26:42 +0200

    wandb passing key positionally

--- scripts/wandb_setup.sh
+++ scripts/wandb_setup.sh
@@ -7,4 +7,4 @@ source "${SCRIPT_DIR}/lib/secret_helpers.sh"
 pip install -U wandb
 
 api_key="$(op_read_secret "${OP_WANDB_KEY_REF:-}" "Weights & Biases API key")"
-wandb login --relogin --cloud --apikey "$api_key" >/dev/null
+wandb login --relogin --cloud "$api_key" >/dev/null
